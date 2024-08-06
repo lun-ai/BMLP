a61697c6a681e7ab869aff18c9009b4b1504e88b
Lun Ai <allenai1997@vip.sina.com>
2024-08-06 17:26:41 +0100

    task2 results

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ src/
 
 ## Evaluation
 
-```bash run_exp.sh system_name experiments/networks/connect sub_folder```
+```bash run_exp.sh SYSTEM_NAME experiments/networks/connect/SUB_FOLDER REP```
 
-Alternative systems are needed for comparison: 
-- bpl:   B-Prolog (binary)
-- xsbpl: XSB-Prolog (binary, can re-make if not usable by following the XSB/doc/userman/manual1.pdf)
+Alternative systems are needed for comparison (system_alias: system and installation):
+- all:   run all
+- bpl:   B-Prolog (binary in BProlog/)
+- xsbpl: XSB-Prolog (binary in XSB/bin, can re-make if not usable by following the XSB/doc/userman/manual1.pdf)
 - swipl: SWI-Prolog (Install from https://www.swi-prolog.org/Download.html)
-- 
+- clingo: Clingo (install from https://github.com/potassco/clingo/releases/)
+- souffle: Souffle (binary in Souffle/ or following instructions from https://souffle-lang.github.io/build)
 
 ### Experimental data
 
--- run_exp.sh
+++ run_exp.sh
@@ -6,9 +6,14 @@ trap '' HUP INT
 # bash run_exp.sh system_name experiments/networks/connect sub_folder
 
 ############################## connect ####################################
-method=$1
-repo=${2}/${3}
+repo=${2}
 cur_dir=$(pwd)
+
+if [[ $1 == "all" ]]; then
+  methods=("bpl" "xsbpl")
+else
+  methods=(${1})
+fi
 
 if [[ $3 == "partial" ]]; then
   nodes=(1000 2000 3000 4000 5000)
@@ -16,60 +21,59 @@ if [[ $3 == "partial" ]]; then
   p=(0.001)
 else
   nodes=(5000)
-#  p=(0.0001 0.001 0.01 0.1 0.5 1)
-  p=(0.5 1)
+  p=(0.0001)
+#  p=(0.5 1)
 fi
 
-for k in "${nodes[@]}"
-do
-  for j in "${p[@]}"
-  do
-      for i in $(seq 1 10)
-      do
+for k in "${nodes[@]}"; do
+  for j in "${p[@]}"; do
+      for i in $(seq 1 ${3}); do
         swipl -s experiments/generate_BK.pl -g "generate_background($j,$k,'${repo}'),halt" -q
-        case $method in
-  # BMLP
-          bmlp-smp)
-          swipl -s ${repo}/swi.pl -t 'test(smp)' -q
-          ;;
-#          bmlp-ime)
-#          swipl -s ${repo}/swi.pl -t 'test(ime)' -q
-#          ;;
-          bmlp-rms)
-          swipl -s ${repo}/swi.pl -t 'test(rms)' -q
-          ;;
-  # SWI-Prolog
-          swipl)
-          swipl -s ${repo}/swi.pl -t 'test(swipl)' -q
-          ;;
-  # B-Prolog
-          bpl)
-          cd ${repo}
-          ${cur_dir}/experiments/BProlog/bp -s 40000000 -g "consult(b_prolog),compute" | sed -n "4p"
-          cd ${cur_dir} - > /dev/null
-          ;;
-  # XSB-Prolog
-          xsbpl)
-          cd ${repo}
-          ${cur_dir}/experiments/XSB/bin/xsb -e "consult(xsb_prolog),compute." --quietload | sed 's/^.* | ?- //'
-          cd ${cur_dir} > /dev/null
-          ;;
-  # Clingo
-          clg)
-          cp ${repo}/background.pl ${repo}/background.lp
-          clingo -q --time-limit=5000 ${repo}/background.lp ${repo}/clingo.lp | sed -n "9p" | sed 's/^.*: //' | sed 's/.$//'
-          ;;
-  # Souffle
-          souffle)
-          swipl -s ${repo}/generate_BK.pl -g "conversion_background_to_dl,halt" -q
-          cd ${repo}
-          souffle -c -F . -D . souffle.dl -p souffle.log
-          souffleprof souffle.log -j=${j}pe_${k}nodes.html > /dev/null
-          cat ${j}pe_${k}nodes.html | grep "data={" | sed 's/.*\[//' | sed 's/,.*//'
-          rm -f *.html *.facts *.csv *.log *.cpp
-          cd ../../../../
-          ;;
-        esac
-      done>./${method}_${j}pe_${k}nodes.txt
+        for method in "${methods[@]}"; do
+          case $method in
+    # BMLP
+            bmlp-smp)
+            swipl -s ${repo}/swi.pl -t 'test(smp)' -q
+            ;;
+  #          bmlp-ime)
+  #          swipl -s ${repo}/swi.pl -t 'test(ime)' -q
+  #          ;;
+            bmlp-rms)
+            swipl -s ${repo}/swi.pl -t 'test(rms)' -q
+            ;;
+    # SWI-Prolog
+            swipl)
+            swipl -s ${repo}/swi.pl -t 'test(swipl)' -q
+            ;;
+    # B-Prolog
+            bpl)
+            cd ${repo}
+            ${cur_dir}/experiments/BProlog/bp -s 40000000 -g "consult(b_prolog),compute" | sed -n "4p"
+            cd ${cur_dir} > /dev/null
+            ;;
+    # XSB-Prolog
+            xsbpl)
+            cd ${repo}
+            ${cur_dir}/experiments/XSB/bin/xsb -e "consult(xsb_prolog),compute." --quietload | sed 's/^.* | ?- //'
+            cd ${cur_dir} > /dev/null
+            ;;
+    # Clingo
+            clg)
+            cp ${repo}/background.pl ${repo}/background.lp
+            clingo -q --time-limit=5000 ${repo}/background.lp ${repo}/clingo.lp | sed -n "9p" | sed 's/^.*: //' | sed 's/.$//'
+            ;;
+    # Souffle
+            souffle)
+            swipl -s ${repo}/generate_BK.pl -g "conversion_background_to_dl,halt" -q
+            cd ${repo}
+            ${cur_dir}/experiments/Souffle/souffle -c -F . -D . souffle.dl -p souffle.log
+            ${cur_dir}/experiments/Souffle/souffleprof souffle.log -j=${j}pe_${k}nodes.html > /dev/null
+            cat ${j}pe_${k}nodes.html | grep "data={" | sed 's/.*\[//' | sed 's/,.*//'
+            rm -f *.html *.facts *.csv *.log *.cpp
+            cd ${cur_dir} > /dev/null
+            ;;
+          esac>>./${method}_${j}pe_${k}nodes.txt
+        done
+      done
   done
 done
