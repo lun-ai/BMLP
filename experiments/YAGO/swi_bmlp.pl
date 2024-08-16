6c02f1abf0438fdd55f028b79ffc96e0a618ffb3
Lun Ai <allenai1997@vip.sina.com>
2024-08-16 18:47:35 +0100

    rearrange datasets

--- experiments/YAGO/swi_bmlp.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(bmlp).
-
-compute :-
-        init('./temp'),
-        compile('experiments/connect/full/background.pl',db(edge,[node,node]),M1),
-        call_time(compute(rms,M1,_),Stats),
-        get_dict(cpu,Stats,CpuT),
-        writeln(CpuT),halt.
